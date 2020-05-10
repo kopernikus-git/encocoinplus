71fc517341cc9199c4c0e3f544ff2477c1c5ba2a
kopernikus-git <65019294+kopernikus-git@users.noreply.github.com>
2020-05-10 19:15:52 +0200

    Define QT_NO_KEYWORDS

--- src/qt/epgc/settings/settingsinformationwidget.h
+++ src/qt/epgc/settings/settingsinformationwidget.h
@@ -23,7 +23,7 @@ public:
 
     void loadClientModel() override;
 
-private slots:
+private Q_SLOTS:
     void setNumConnections(int count);
     void setMasternodeCount(const QString& strMasternodes);
     void setNumBlocks(int count);
