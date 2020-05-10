e6902e6fe0f3624f85b91e05f1d0e28e6849e4ec
kopernikus-git <65019294+kopernikus-git@users.noreply.github.com>
2020-05-10 20:23:48 +0200

    Define QT_NO_KEYWORDS

--- src/qt/zepgcontroldialog.h
+++ src/qt/zepgcontroldialog.h
@@ -57,7 +57,7 @@ private:
     };
     friend class CZEpgControlWidgetItem;
 
-private slots:
+private Q_SLOTS:
     void updateSelection(QTreeWidgetItem* item, int column);
     void ButtonAllClicked();
 };
