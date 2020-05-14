bc7603a8efb4cb32d7394eb667dd0219b3fb792b
kopernikus-git <65019294+kopernikus-git@users.noreply.github.com>
2020-05-14 20:31:36 +0200

    Wait locking for genesis connection

--- src/main.cpp
+++ src/main.cpp
@@ -5067,8 +5067,6 @@ bool InitBlockIndex()
             if (!WriteBlockToDisk(block, blockPos))
                 return error("LoadBlockIndex() : writing genesis block to disk failed");
             CBlockIndex* pindex = AddToBlockIndex(block);
-            if (!ReceivedBlockTransactions(block, state, pindex, blockPos))
-                return error("LoadBlockIndex() : genesis block not accepted");
             if (!ActivateBestChain(state, &block))
                 return error("LoadBlockIndex() : genesis block cannot be activated");
             // Force a chainstate write so that when we VerifyDB in a moment, it doesnt check stale data
