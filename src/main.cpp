7a33f327954f9cdfd7a8802b495f053189a56575
Kopernikus-Git <63426249+Kopernikus1@users.noreply.github.com>
2020-06-04 19:11:19 +0200

    Sync Upgrades, part 2.
    Sync Upgrades, part 2.  # 1336

--- src/main.cpp
+++ src/main.cpp
@@ -3053,6 +3053,12 @@ static bool ActivateBestChainStep(CValidationState& state, CBlockIndex* pindexMo
  */
 bool ActivateBestChain(CValidationState& state, CBlock* pblock, bool fAlreadyChecked)
 {
+    // Note that while we're often called here from ProcessNewBlock, this is
+    // far from a guarantee. Things in the P2P/RPC will often end up calling
+    // us in the middle of ProcessNewBlock - do not assume pblock is set
+    // sanely for performance or correctness!
+    AssertLockNotHeld(cs_main);
+
     CBlockIndex* pindexNewTip = nullptr;
     CBlockIndex* pindexMostWork = nullptr;
     do {
@@ -4149,6 +4155,8 @@ void CBlockIndex::BuildSkip()
 
 bool ProcessNewBlock(CValidationState& state, CNode* pfrom, CBlock* pblock, CDiskBlockPos* dbp)
 {
+   AssertLockNotHeld(cs_main);
+   
     // Preliminary checks
     int64_t nStartTime = GetTimeMillis();
 
@@ -4970,6 +4978,8 @@ bool static AlreadyHave(const CInv& inv)
 
 void static ProcessGetData(CNode* pfrom)
 {
+    AssertLockNotHeld(cs_main);
+
     std::deque<CInv>::iterator it = pfrom->vRecvGetData.begin();
 
     std::vector<CInv> vNotFound;
